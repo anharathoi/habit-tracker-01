175edc43df5e157fe035d27b550f90b085f5d2ce
anharathoit <anharzihan@gmail.com>
2020-07-27 22:01:15 +1000

    Basic table structure for trackers component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TrackerComponent } from './tracker/tracker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TrackerComponent
   ],
   imports: [
     BrowserModule,
