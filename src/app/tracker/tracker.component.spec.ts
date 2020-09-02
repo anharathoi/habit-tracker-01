40d3c6823f41c97a001321e744391f6cb354a552
anharathoit <anharzihan@gmail.com>
2020-09-02 21:42:38 +1000

    Fixed existing tests.

--- src/app/tracker/tracker.component.spec.ts
+++ src/app/tracker/tracker.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TrackerComponent } from './tracker.component';
 
@@ -8,7 +9,8 @@ describe('TrackerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TrackerComponent ]
+      declarations: [ TrackerComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   }));
